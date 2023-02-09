509f4550df798568f7f2f589a7157ead002dd04d
Mike Johnson <mike@mapleprintservices.com>
2023-02-09 17:36:05 -0500

    service worker added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,6 +29,26 @@ module.exports = () => {
       }),
       new MiniCssExtractPlugin(),
       new CleanWebpackPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './service-worker.js',
+      }),
+      // new WebpackPwaManifest({
+      //   name: 'Just Another Text Editor',
+      //   short_name: 'JATE',
+      //   description: 'Edit text on or offline',
+      //   background_color: '#7eb4e2',
+      //   theme_color: '#7eb4e2',
+      //   start_url: './',
+      //   publicPath: './',
+      //   icons: [
+      //     {
+      //       src: path.resolve('./src/images/logo.png'),
+      //       sizes: [96, 128, 192, 256, 384, 512],
+      //       destination: path.join('assets', 'icons'),
+      //     },
+      //   ],
+      // }),
     ],
     module: {
       rules: [
