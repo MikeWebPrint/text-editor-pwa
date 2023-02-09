76bd35161ea5bb52fa9075429e393d5b8c98a816
Mike Johnson <mike@mapleprintservices.com>
2023-02-09 14:00:19 -0500

    build processes working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -8,23 +9,58 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    devServer: {
+      hot: "only",
+    },
     output: {
-      filename: '[name].bundle.js',
+      filename: '[name].bundle.[contenthash].js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        title: "Text Editor",
+        template: "./index.html"
+      }),
+      new MiniCssExtractPlugin(),
 
+    ],
     module: {
       rules: [
-        
+        {
+          test: /\.html$/i,
+          use: ["html-loader"],
+          // options: {
+          //   // Disables attributes processing
+          //   sources: false,
+          // },
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
+
+
+
   };
 };
