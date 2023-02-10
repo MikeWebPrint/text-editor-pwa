e9379abb08c7eda8dd39525e440052d19498a224
Mike Johnson <mike@mapleprintservices.com>
2023-02-10 01:44:13 -0500

    updated client package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,8 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
